33d844502ee5ba5f0f43c34443ea0d2d85905533
Thanh0721 <thanhn2789@nycstudents.net>
2025-01-14 17:24:11 +0000

    final

--- Main.java
+++ Main.java
@@ -1,2 +1,25 @@
 public class Main {
+    public static void main(String[] args) 
+    {
+        Candy[][] candies = 
+        {
+            {null, new Candy("cherry"), null},
+            {new Candy("lime"), null, new Candy("grape")},
+            {new Candy("cherry"), new Candy("lime"), null}
+        };
+
+        BoxOfCandy box = new BoxOfCandy(candies);
+
+        System.out.println("Initial Box:");
+        System.out.println(box);
+
+        System.out.println("Move Candy to First Row, Column 0: " + box.moveCandyToFirstRow(0));
+        System.out.println(box);
+
+        System.out.println("Remove Candy with flavor 'lime': " + box.removeNextByFlavor("lime"));
+        System.out.println(box);
+
+        System.out.println("Remove Candy with flavor 'grape': " + box.removeNextByFlavor("grape"));
+        System.out.println(box);
+    }
 }
