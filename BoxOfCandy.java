ba5200160d1d6d5689f5d1800cdc6addd6c389c6
Thanh0721 <thanhn2789@nycstudents.net>
2025-01-14 01:20:53 +0000

    add.main

--- BoxOfCandy.java
+++ BoxOfCandy.java
@@ -1,23 +1,49 @@
-public class BoxOfCandy
-{
-    /** box contains at least one row and is initialized in the constructor. */
+public class BoxOfCandy {
     private Candy[][] box;
 
-    /**
-    * Moves one piece of candy in column col, if necessary and possible, so that the box
-    * element in row 0 of column col contains a piece of candy, as described in part (a).
-    * Returns false if there is no piece of candy in column col and returns true otherwise.
-    * Precondition: col is a valid column index in box.
-    */
-    public boolean moveCandyToFirstRow(int col)
-    { /* to be implemented in part (a) */ }
+    public BoxOfCandy(Candy[][] box) {
+        this.box = box;
+    }
 
-    /**
-    * Removes from box and returns a piece of candy with flavor specified by the parameter, or
-    * returns null if no such piece is found, as described in part (b)
-    */
-    public Candy removeNextByFlavor(String flavor)
-    { /* to be implemented in part (b) */ }
+    public boolean moveCandyToFirstRow(int col) {
+        if (box[0][col] != null) {
+            return true;
+        }
 
-    // There may be instance variables, constructors, and methods that are not shown.
+        for (int i = 1; i < box.length; i++) {
+            if (box[i][col] != null) {
+                box[0][col] = box[i][col];
+                box[i][col] = null;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public Candy removeNextByFlavor(String flavor) {
+        for (int i = box.length - 1; i >= 0; i--) {
+            for (int j = 0; j < box[i].length; j++) {
+                if (box[i][j] != null && box[i][j].getFlavor().equals(flavor)) {
+                    Candy foundCandy = box[i][j];
+                    box[i][j] = null;
+                    return foundCandy;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Candy[] row : box) {
+            for (Candy candy : row) {
+                sb.append(candy != null ? candy : "null").append(" ");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
 }
--- Candy.java
+++ Candy.java
@@ -1,9 +1,16 @@
 public class Candy {
+    private String flavor;
 
-    /** Returns a String representing the flavor of this piece of candy */
+    public Candy(String flavor) {
+        this.flavor = flavor;
+    }
+
     public String getFlavor() {
-        /* implementation not shown */ }
+        return flavor;
+    }
 
-    // There may be instance variables, constructors, and methods that are not
-    // shown.
+    @Override
+    public String toString() {
+        return flavor;
+    }
 }
